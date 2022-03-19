b642f4e387473e294ef70368831dc16e1bc28350
Jose S <stricklin.jose@gmail.com>
2022-03-18 19:22:33 -0600

    update working card gen

--- Assets/Code/Classes/Card.cs
+++ Assets/Code/Classes/Card.cs
@@ -10,4 +10,19 @@ public class Card
     public Suit suit;
     public int val;
     public Sprite face;
+    Transform transform;
+
+    public Card(Suit suit, int val, Sprite face, GameObject go)
+    {
+        this.suit = suit;
+        this.val = val;
+        this.face = face;
+        this.transform = go.transform;
+        go.GetComponent<Image>().sprite = face;
+    }
+    public void SetParent(Transform parent)
+    {
+        transform.SetParent(parent);
+    }
+
 }
--- Assets/Code/Managers/CardManager.cs
+++ Assets/Code/Managers/CardManager.cs
@@ -1,19 +1,49 @@
 using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
     [SerializeField] Card[] cards;
+    [SerializeField] RectTransform container;
+    [SerializeField] Dictionary<Suit, List<Card>> renderCards = new Dictionary<Suit, List<Card>>();
+    [SerializeField] GameObject cardPrefab;
+    List<Card> tempList;
+    Card tempCard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitializeCards();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeCards()
     {
-        
+        // sort card order
+        for (int i = 0; i < cards.Length; i++)
+        {
+
+        }
+
+            // generate cards
+            for (int i = 0; i < cards.Length; i++)
+        {
+            tempCard = new Card(cards[i].suit, cards[i].val, cards[i].face, Instantiate(cardPrefab));
+            tempCard.SetParent(container);
+            
+            if (renderCards.TryGetValue(cards[i].suit, out List<Card> cardList))
+            {
+                cardList.Add(tempCard);
+                renderCards[cards[i].suit] = cardList;
+            } else
+            {
+                tempList = new List<Card>();
+
+                tempList.Add(tempCard);
+                renderCards[cards[i].suit] = tempList;
+            }
+        }
     }
 }
